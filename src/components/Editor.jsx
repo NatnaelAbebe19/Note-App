335e18f263fa8017cbdeffe60e62a7f92fefd72a
jud <joejud@gmail.com>
2024-05-15 22:26:11 +0300

    update for the sake of deployment

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,31 +1,32 @@
-import React from "react"
-import ReactMde from "react-mde"
+import React from "react";
+import ReactMde from "react-mde";
+import "react-mde/lib/styles/css/react-mde-all.css";
 // const ReactMde = R.default
-import Showdown from "showdown"
+import Showdown from "showdown";
 
 export default function Editor({ tempNoteText, setTempNoteText }) {
-    const [selectedTab, setSelectedTab] = React.useState("write")
+  const [selectedTab, setSelectedTab] = React.useState("write");
 
-    const converter = new Showdown.Converter({
-        tables: true,
-        simplifiedAutoLink: true,
-        strikethrough: true,
-        tasklists: true,
-    })
+  const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+  });
 
-    return (
-        <section className="pane editor">
-            <ReactMde
-                value={tempNoteText}
-                onChange={setTempNoteText}
-                selectedTab={selectedTab}
-                onTabChange={setSelectedTab}
-                generateMarkdownPreview={(markdown) =>
-                    Promise.resolve(converter.makeHtml(markdown))
-                }
-                minEditorHeight={80}
-                heightUnits="vh"
-            />
-        </section>
-    )
+  return (
+    <section className="pane editor">
+      <ReactMde
+        value={tempNoteText}
+        onChange={setTempNoteText}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+        minEditorHeight={80}
+        heightUnits="vh"
+      />
+    </section>
+  );
 }
